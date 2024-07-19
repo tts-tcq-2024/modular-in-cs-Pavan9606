3c4c70d1f56e0494d1d23e06e475d442c63a252e
Pavan9606 <118717558+Pavan9606@users.noreply.github.com>
2024-07-19 17:05:49 +0530

    Update ColorPair.cs

--- ColorPair.cs
+++ ColorPair.cs
@@ -4,8 +4,14 @@ namespace TelCo.ColorCoder
 {
     internal class ColorPair
     {
-        internal Color majorColor;
-        internal Color minorColor;
+        internal Color majorColor { get; }
+        internal Color minorColor { get; }
+
+        public ColorPair(Color MajorColor, Color MinorColor)
+        {
+            majorColor = MajorColor;
+            minorColor = MinorColor;
+        }
         public override string ToString()
         {
             return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
